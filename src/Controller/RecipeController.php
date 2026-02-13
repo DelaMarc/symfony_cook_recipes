9207225dea1234f7f9539ce81cba21e7c5d532e3
Marc D <marcd@break-first.com>
2026-02-13 21:14:02 +0100

    (php, twig, env) created database for recipes and display queried reciped from controller using prebuit queries

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -7,27 +7,34 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTimeImmutable;
 
 final class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index')]
-    public function ondex(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findWithDurationLowerThan(20);
+        //dd($recipes);
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
 
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
-
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() != $slug){
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+        //dd($recipe);
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
-            'person' => [
-                'firstname' => 'John',
-                'lastname' => 'Doe',
-            ]
+            'recipe' => $recipe,
         ]);
     }
 }
